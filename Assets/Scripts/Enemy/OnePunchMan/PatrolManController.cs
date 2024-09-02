5a5ff13f1b1232921daed6c90c46667652397721
Yashvardhan4197 <20BCS4197@cuchd.in>
2024-09-02 12:28:44 +0530

    Added PatrolManController+PatrolManStateMachine

---
+++ Assets/Scripts/Enemy/OnePunchMan/PatrolManController.cs
@@ -0,0 +1,35 @@
+﻿
+using StatePattern.Player;
+using UnityEngine;
+
+namespace StatePattern.Enemy
+{
+    public class PatrolManController : EnemyController
+    {
+        private IStateMachine stateMachine;
+        public PatrolManController(EnemyScriptableObject enemyScriptableObject) : base(enemyScriptableObject)
+        {
+            enemyView.SetController(this);
+            stateMachine = new PatrolManStateMachine(this);
+            stateMachine.ChangeState(States.IDLE);
+        }
+
+        public override void UpdateEnemy()
+        {
+            if (currentState == EnemyState.DEACTIVE)
+                return;
+            stateMachine.Update();
+        }
+
+        public override void PlayerEnteredRange(PlayerController targetToSet)
+        {
+            Debug.Log("hello");
+            base.PlayerEnteredRange(targetToSet);
+            stateMachine.ChangeState(States.CHASING);
+        }
+
+        public override void PlayerExitedRange() => stateMachine.ChangeState(States.IDLE);
+
+
+    }
+}
---
+++ Assets/Scripts/Enemy/OnePunchMan/StateMachine/PatrolManStateMachine.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StatePattern.Enemy
+{
+    public class PatrolManStateMachine : IStateMachine
+    {
+        public EnemyController Owner { get; private set; }
+        private IState currentState;
+        protected Dictionary<States, IState> StateList = new Dictionary<States, IState>();
+
+        public PatrolManStateMachine(EnemyController owner)
+        {
+            Owner = owner;
+            CreateStates();
+            SetOwner();
+        }
+        public void ChangeState(States newState)
+        {
+            currentState?.OnStateExit();
+            currentState = StateList[newState];
+            currentState?.OnStateEnter();
+        }
+
+        public void CreateStates()
+        {
+            StateList.Add(States.IDLE, new IdleState(this));
+            StateList.Add(States.ROTATING, new RotatingState(this));
+            StateList.Add(States.SHOOTING, new ShootingState(this));
+            StateList.Add(States.PATROLLING, new PatrollingState(this));
+            StateList.Add(States.CHASING, new ChasingState(this));
+        }
+
+        public void SetOwner()
+        {
+            foreach(IState state in StateList.Values)
+            {
+                state.Owner = Owner;
+            }
+        }
+
+        public void Update()
+        {
+            currentState?.Update();
+        }
+    }
+}
