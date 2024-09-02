10d54aad8c998e09ef290fd472ac2a9ec038ffe8
Yashvardhan4197 <20BCS4197@cuchd.in>
2024-09-02 14:01:20 +0530

    Added GenericStateMachine logic

---
+++ Assets/Scripts/Enemy/GenericStateMachine.cs
@@ -0,0 +1,35 @@
+﻿using StatePattern.StateMachine;
+using System;
+using System.Collections.Generic;
+
+namespace StatePattern.Enemy
+{
+    public class GenericStateMachine<T>where T:EnemyController
+    {
+        public T Owner { get; set; }
+        public IState currentState;
+        protected Dictionary<States,IState> States=new Dictionary<States,IState>();
+        public GenericStateMachine(T owner)=>this.Owner=owner;
+        public void ChangeState(States state) => changeState(state);
+
+        protected void SetOwner()
+        {
+            foreach(var state in States.Values)
+            {
+                state.Owner = Owner;
+            }
+        }
+        protected void changeState(States newState)
+        {
+            currentState?.OnStateExit();
+            currentState = States[newState];
+            currentState?.OnStateEnter();
+        }
+
+        public void Update()
+        {
+            currentState?.Update();
+        }
+
+    }
+}
